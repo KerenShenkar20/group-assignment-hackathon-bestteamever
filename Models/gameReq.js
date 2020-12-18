f88a6057a0add564d84fca80a2b50307e7f437bc
dekelme <61948586+dekelme@users.noreply.github.com>
2020-12-18 11:56:32 +0200

    Update gameReq.js

--- Models/gameReq.js
+++ Models/gameReq.js
@@ -2,6 +2,7 @@ const { ObjectID } = require('bson');
 const { Schema, model } = require('mongoose');
 
 const gameReqSchema = new Schema({
+    userId: { type: Number },
 }, {collection: 'gameReqs'});
 const GameReq = model('GameReq', gameReqSchema);
 module.exports = GameReq;
