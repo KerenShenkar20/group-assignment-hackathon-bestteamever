bb8d0d4597bd56b5ec1c44bfa21a92858496ddaf
DAVID <tsiba1@walla.co.il>
2020-12-18 12:22:55 +0200

    conection

--- Controllers/gameReq.ctrl.js
+++ Controllers/gameReq.ctrl.js
@@ -1,1 +1,55 @@
-///
+///Gamerequests CRUD controler
+
+
+const GameReq = require('../models/gameReq');
+
+exports.gameReqontroller = {
+    getReq(req, res) {
+        const id = req.params.id
+        GameReq.find({ id: id }).
+            then(docs => { res.json(docs) })
+            .catch(err => console.log(`Erorr getting the data from db: ${err}`));
+    },
+    getReqs(req, res) {
+        GameReq.find({})
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log('Error getting the data from db: ${err}'));
+
+    },
+
+    addReq(req, res) {
+        const newUser = new GameReq(req.body);
+        const result = newUser.save()
+            .then(result => {
+                if (result) {
+                    res.json(result)
+        
+                }
+                else {
+                    res.status(404).send("Error saving a user");
+                }
+            })
+            .catch(err => console.log('Error saving the data from db: ${err}'))
+        
+
+
+    },
+    updateReq(req, res) {
+        GameReq.updateOne({ id: req.params.id }, {
+            userId: req.params.id,
+            date: req.body.date,
+            time: req.body.time,
+        })
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error update user from db : ${req.params.id}`));
+    },
+
+    deleteReq(req, res) {
+        GameReq.deleteOne({ id: req.params.id })
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error deleting user from db : ${req.params.id}`));
+    }
+}
+
+
+
--- Models/gameReq.js
+++ Models/gameReq.js
@@ -2,9 +2,9 @@ const { ObjectID } = require('bson');
 const { Schema, model } = require('mongoose');
 
 const gameReqSchema = new Schema({
-    userId: { type: Number },
-    date: { type: Date},
-    time: { type: Number},
+    userId: { type: Number, required:true  },
+    date: { type: Date, required:true },
+    time: { type: Number, required:true },
 }, {collection: 'gameReqs'});
 const GameReq = model('GameReq', gameReqSchema);
 module.exports = GameReq;
