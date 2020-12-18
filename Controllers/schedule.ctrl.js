6206f65495c38539be5652ca983d5566ae4d9012
dekelme <61948586+dekelme@users.noreply.github.com>
2020-12-18 12:11:47 +0200

    change model, schedule.ctrl

--- Controllers/schedule.ctrl.js
+++ Controllers/schedule.ctrl.js
@@ -1,1 +1,38 @@
-///
+const User = require('../models/schedule');
+
+exports.scheduleDbcontroller = {
+    getSchedule(req, res) {
+        const id = req.params.id
+        User.find({ id: id }).
+            then(docs => { res.json(docs) })
+            .catch(err => console.log('Erorr getting the data from db: ${err}'));
+    },
+    getSchedule(req, res) {
+        let filter= { };
+        if('date' in req.query)
+            filter.date=req.query.date;
+        if('time' in req.query)
+            filter.time= req.query.time;
+        User.find(filter)
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log('Error getting the data from db: ${err}'));
+
+    },
+
+    addSchedule(req, res) {
+        const newSchedule = new Schedule(req.body);
+        const result = newSchedule.save()
+            .then(result => {
+                if (result) {
+                    res.json(result)
+        
+                }
+                else {
+                    res.status(404).send("Error saving a user");
+                }
+            })
+            .catch(err => console.log('Error saving the data from db: ${err}'))
+        
+
+
+    },
--- Models/gameReq.js
+++ Models/gameReq.js
@@ -3,6 +3,8 @@ const { Schema, model } = require('mongoose');
 
 const gameReqSchema = new Schema({
     userId: { type: Number },
+    date: { type: Date},
+    time: { type: Number},
 }, {collection: 'gameReqs'});
 const GameReq = model('GameReq', gameReqSchema);
 module.exports = GameReq;
