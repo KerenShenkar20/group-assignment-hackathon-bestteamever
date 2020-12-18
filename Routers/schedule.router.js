c3af77c0ead93fcd86a88325c49e2fd0f4f09ddf
yalisofer94 <yalisofer@gmail.com>
2020-12-18 13:08:45 +0200

    deploy

--- Routers/schedule.router.js
+++ Routers/schedule.router.js
@@ -3,9 +3,9 @@ const { scheduleDbcontroller } = require('../Controllers/schedule.ctrl');
 
 const scheduleRouter= new Router;
 
-scheduleRouter.get('/', scheduleDbcontroller.getSchedules);
+//scheduleRouter.get('/', scheduleDbcontroller.getSchedules);
 scheduleRouter.get('/:id', scheduleDbcontroller.getSchedule);
-scheduleRouter.post('/', scheduleDbcontroller.addSchedule);
+//scheduleRouter.post('/', scheduleDbcontroller.addSchedule);
 scheduleRouter.put('/:id', scheduleDbcontroller.updateSchedule);
 scheduleRouter.delete('/:id', scheduleDbcontroller.deleteSchedule);
 
