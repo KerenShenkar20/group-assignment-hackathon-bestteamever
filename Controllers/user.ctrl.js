0947249a7c6389ffec914d0ded0e0fdda1e38e71
Moriel Turjeman <moriel99@pop-os.localdomain>
2020-12-18 12:10:42 +0200

    getUser and getUsers CRUD

--- Controllers/user.ctrl.js
+++ Controllers/user.ctrl.js
@@ -1,1 +1,22 @@
-// Change the name of the file if needed
+const User = require('../Models/user');
+
+exports.userDbcontroller={
+    getUser(req, res){
+        const id= req.params.id
+        User.find({id:id})
+        .then(docs=>{res.json(docs)})
+        .catch(err=>console.log(`Erorr getting the data from db: ${err}`));
+    },
+
+    getUsers(req, res){
+        let filter={ };
+        if('email' in req.query)
+            filter.email=req.query.email;
+        User.find(filter)
+            .then(docs=>{res.json(docs)})
+            .catch(err=>console.log(`Error getting the data from db: ${err}`));
+
+    },
+    
+
+}
